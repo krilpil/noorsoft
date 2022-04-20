271696a6e0b4d8d49910c0e23eef54a1589ecf13
Kirill Bespalov <kesha_bespalov@mail.ru>
2022-04-20 23:47:09 +0300

    Initializing a React project with TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = (): JSX.Element => {
+    return (
+        <h1>Hello React project with: TS, SCSS Module!</h1>
+    );
+}
+
+export default React.memo(App);
