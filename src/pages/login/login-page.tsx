7394470a736aea871bed4ebfe44012d4563551cb
Kirill Bespalov <kesha_bespalov@mail.ru>
2022-04-21 12:31:32 +0300

    Binding Formik to a form using the useFormik hook

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import {useFormik} from 'formik';
 import {LockOutlined, UserOutlined} from "@ant-design/icons";
 import {LoginPageWrapper, Form, Title, Paragraph, Input, Password, Button} from './components'
 
 const LoginPage = (): JSX.Element => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        onSubmit: values => {
+            console.log(values)
+        },
+    })
+
     return (
         <LoginPageWrapper>
-            <Form>
+            <Form onSubmit={formik.handleSubmit}>
                 <Title level={1}>Noorsoft</Title>
                 <Paragraph>Enter your email and password.</Paragraph>
-                <Input placeholder="Email" prefix={<UserOutlined/>}/>
-                <Password placeholder="Password" prefix={<LockOutlined/>}/>
-                <Button block>Login</Button>
+                <Input
+                    id={'email'}
+                    name={'email'}
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                    placeholder="Email"
+                    prefix={<UserOutlined/>}
+                />
+                <Password
+                    id={'password'}
+                    name={'password'}
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    placeholder="Password"
+                    prefix={<LockOutlined/>}
+                />
+                <Button htmlType={'submit'} block>Login</Button>
             </Form>
         </LoginPageWrapper>
     );
