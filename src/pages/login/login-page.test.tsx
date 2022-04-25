de1182a90ee49c9ea92cffee2392a4766d8339a2
Kirill Bespalov <kesha_bespalov@mail.ru>
2022-04-25 21:29:51 +0300

    Added a test for the login-page component using enzyme

---
+++ src/pages/login/login-page.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {mount} from 'enzyme'
+import LoginPage from "./login-page";
+import rootReducer from "../../redux/reducers";
+import {Provider} from "react-redux";
+import {createStore} from "redux"
+
+const initialState: any = {
+    form: {
+        request: false,
+        user: {}
+    }
+}
+
+const store = createStore(rootReducer, initialState);
+
+describe('Login page', () => {
+    let wrapper: any
+
+    beforeEach(() => {
+        const Wrapper = ({children}: any) => (
+            <Provider store={store}>{children}</Provider>
+        );
+
+        wrapper = mount(<LoginPage/>, {wrappingComponent: Wrapper})
+    })
+
+    it('should render something', async () => {
+        expect(wrapper).not.toBeNull()
+    })
+
+    it('should render paragraph', async () => {
+        expect(wrapper.find('p').text()).toEqual('Enter your email and password.')
+    })
+})
