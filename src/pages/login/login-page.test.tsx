9a11f5da647860ee5d3f6d82ba069537c7435021
Kirill Bespalov <kesha_bespalov@mail.ru>
2022-04-27 17:39:28 +0300

    Initializing ts-jest

--- src/pages/login/login-page.test.tsx
+++ src/pages/login/login-page.test.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import {mount} from 'enzyme'
+import Enzyme, {mount} from 'enzyme'
 import LoginPage from "./login-page";
 import rootReducer from "../../redux/reducers";
 import {Provider} from "react-redux";
 import {createStore} from "redux"
+import 'jsdom-global/register';
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+Enzyme.configure({adapter: new Adapter()})
 
 const initialState: any = {
     form: {
